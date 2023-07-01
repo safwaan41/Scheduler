a1cbbd598e22268829ca29017ade1d3fe3c5d53b
safwaan41 <safwaan41@gmail.com>
2023-06-30 23:44:00 -0500

    final

--- Develop/script.js
+++ Develop/script.js
@@ -1,86 +1,96 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  //show date on header
+$(function () 
+{
 var today = dayjs().format("ddd, MMM DD YYYY");
-var now = dayjs().format("H A");
+//show date on header
 // console.log(today);
 $("#currentDay").text(today);
 
 //show time blocks dynamically
-var planWorkday = [
-  { time: "9 AM", 
+var dayPlan = 
+[
+  { time: "9 AM", id:9, 
       event: "" },
-  { time: "10 AM", 
+  { time: "10 AM", id:10, 
       event: "" },
-  { time: "11 AM", 
+  { time: "11 AM", id:11, 
       event: "" },
-  { time: "12 PM", 
+  { time: "12 PM", id:12, 
       event: "" },
-  { time: "1 PM", 
+  { time: "1 PM", id:1, 
       event: "" },
-  { time: "2 PM", 
+  { time: "2 PM", id:2, 
       event: "" },
-  { time: "3 PM", 
+  { time: "3 PM", id:3, 
       event: "" },
-  { time: "4 PM", 
+  { time: "4 PM", id:4, 
       event: "" },
-  { time: "5 PM", 
+  { time: "5 PM", id:5, 
       event: "" },
 ];
-// create rows
-planWorkday.forEach(function(timeBlock, index) 
+
+/* Local Storage auto pull*/
+var toDo = JSON.parse(localStorage.getItem("banana"));
+if (toDo) 
 {
-	var timeLabel = timeBlock.time;
-	var blockColor = colorRow(timeLabel);
+  dayPlan = toDo;
+}
+
+// create rows
+dayPlan.forEach(function(hourDay, index) 
+{
+	var label = hourDay.time;
+  var id = hourDay.id;
+	var ppp = color(id);
 	var row =
-		'<div class="time-block" id="' +
+  ///jquery or jqurey UI??
+		'<div class="hour-block" id="' +
 		index +
 		'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
-		timeLabel +
+		label +
 		'</div><textarea class="form-control ' +
-		blockColor +
+		ppp +
 		'">' +
-		timeBlock.event +
+		hourDay.event +
 		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
 
 	/* Adding rows to container div */
 	$(".container").append(row);
 });
+// color each color block
+function color(time) 
+{
+  var currentHour = dayjs().format('h');
+  var futureHour = time;
 
-function colorRow(time) {
-  var planNow = dayjs().format("H A");
-  var planEntry = dayjs().format("H A");
-  // console.log(planNow);
-  // console.log(planEntry)
-	if (planNow.isBefore(planEntry) === true) {
-		return "future";
-	} else if (planNow.isAfter(planEntry) === true) {
-		return "past";
-	} else {
-		return "present";
-	}
+  if (currentHour < futureHour){
+    return "past";
+  }else if(currentHour>futureHour){
+    return "future";
+  }else{return "present"}
 }
 
 
+/// saving to local
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+$(".saveBtn").on("click", function() {
+	var localTimeBlock = parseInt(
+		$(this)
+			.closest(".hour-block")
+			.attr("id")
+	);
+	var inputTodo = $.trim(
+		$(this)
+			.parent()
+			.siblings("textarea")
+			.val()
+	);
+	dayPlan[localTimeBlock].event = inputTodo;
+
+	/* Set local storage */
+	localStorage.setItem("banana", JSON.stringify(dayPlan));
 });
+  });
+
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Scheduler
+# Work Day Scheduler
+Create a work day scheduler that tracks hour by hour 
+Pull in time dynamically via dayjs on load 
+
+## Concepts Used
+Used heavy JS and JS Query
+Dynamically using DayJS for current time and date
+
+## Links to Work
+GitHub: https://github.com/safwaan41/Scheduler
+Deployed website: 
+
+# Screenshot
+![Screenshot](./Assets/Work%20Day%20Scheduler%20SCREENSHOT.png)
+
